78ae48caeb7fe168f2f61aa75f013f90483efb7b
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-07-17 12:44:36 +0300

    gallery

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,51 +4,92 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
-  state ={
+  state = {
     query: '',
     images: [],
     page: 1,
     isEmpty: false,
-  }
+    isVisible: false,
+    isLoading: false,
+    error: null,
+  };
 
-  componentDidUpdate(_, prevState){
-    const {query, page} = this.state;
-    
-    if(prevState.query !== query || prevState.page !== page){
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
       this.getPhotos(query, page);
-      
-
     }
   }
 
-  getPhotos = async(query, page)=>{
-    if(!query)return;
+  getPhotos = async (query, page) => {
+    if (!query) return;
+    this.setState({ isLoading: true });
     try {
-      const {photos, total_results, per_page, page: currentPage} = await ImageService.getImages(query, page);
+      const {
+        photos,
+        total_results,
+        per_page,
+        page: currentPage,
+      } = await ImageService.getImages(query, page);
       console.log(photos, total_results, per_page, currentPage);
-      if(photos.length === 0) {
-        this.setState({isEmpty: true});
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
       }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisible: currentPage < Math.ceil(total_results / per_page),
+      }));
     } catch (error) {
-      
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
     }
-  }
+  };
 
-  onSubmit = (query)=>{
+  onSubmit = query => {
     this.setState({
       query,
       images: [],
       page: 1,
-    })
-  }
+      isEmpty: false,
+      isVisible: false,
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
 
   render() {
-    const {isEmpty} = this.state;
+    const { isEmpty, images, isVisible, isLoading, error } = this.state;
     return (
       <>
-      <SearchForm onSubmit={this.onSubmit}/>
-      {isEmpty &&  <Text textAlign="center">Sorry. There are no images ... 😭</Text>}
-
+        <SearchForm onSubmit={this.onSubmit} />
+        {error && (
+          <Text textAlign="center">❌ Something went wrong - {error}</Text>
+        )}
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        <Grid>
+          {images.map(({ id, avg_color, src, alt }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {isVisible && (
+          <Button onClick={this.onLoadMore}>
+            {isLoading ? 'Loading...' : 'Load More'}
+          </Button>
+        )}
       </>
     );
   }
